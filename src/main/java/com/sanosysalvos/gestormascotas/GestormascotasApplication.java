447d959ace881226f8303ec8b7d02204dc774692
wsk4 <maa.gonzalezva@duocuc.cl>
2026-04-26 15:25:05 -0400

    v1

---
+++ src/main/java/com/sanosysalvos/gestormascotas/GestormascotasApplication.java
@@ -0,0 +1,13 @@
+package com.sanosysalvos.gestormascotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestormascotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestormascotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanosysalvos/gestormascotas/GestormascotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanosysalvos.gestormascotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestormascotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
