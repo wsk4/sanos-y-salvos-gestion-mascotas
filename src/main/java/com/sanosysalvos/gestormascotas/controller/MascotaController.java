2d9035399450c4bdd50c72a768e5f8dd6425f62a
Moonlighpoio <ren.barriga@duocuc.cl>
2026-04-27 23:10:57 -0400

    feat:add-delete
    implementacion delete en service y controller

--- src/main/java/com/sanosysalvos/gestormascotas/controller/MascotaController.java
+++ src/main/java/com/sanosysalvos/gestormascotas/controller/MascotaController.java
@@ -1,14 +1,23 @@
 package com.sanosysalvos.gestormascotas.controller;
 
-import com.sanosysalvos.gestormascotas.model.Mascota;
-import com.sanosysalvos.gestormascotas.service.MascotaService;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.sanosysalvos.gestormascotas.model.Mascota;
+import com.sanosysalvos.gestormascotas.service.MascotaService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v1/mascotas")
@@ -41,5 +50,11 @@ public class MascotaController {
     public ResponseEntity<Mascota> obtenerMascota(@PathVariable Integer id) {
         Mascota mascota = mascotaService.obtenerPorId(id);
         return ResponseEntity.ok(mascota);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarMascota(@PathVariable Integer id) {
+        mascotaService.eliminarMascota(id);
+        
+        return ResponseEntity.noContent().build(); 
     }
 }
